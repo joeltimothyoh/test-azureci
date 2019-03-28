46c43d2736a5ee9dd3bd5600380901d024476795
Joel Timothy Oh <joeltimothyoh@live.com>
2019-03-28 21:36:42 +0800

    try script again!

--- test.ps1
+++ test.ps1
@@ -1,10 +1,13 @@
-try {
+begin {
     $projectName = 'Get-SMARTReport'
-    git clone "https://github.com/joeltimothyoh/$($projectName).git"; if ($LASTEXITCODE) { throw }
-    Push-Location $projectName
-    Get-Location
-    Import-Module "./Modules/$($projectName)/Get-SMARTReport.psm1"
-    Get-SMARTReport
-}catch {
-    throw
+}process {
+    try {
+        git clone "https://github.com/joeltimothyoh/$($projectName).git"; if ($?) { throw }
+        Push-Location $projectName
+        Get-Location
+        Import-Module "./Modules/$($projectName)/Get-SMARTReport.psm1"
+        Get-SMARTReport
+    }catch {
+        throw
+    }
 }
