46657b8df07e0ed8addfb6e96b13ddfd213177f2
Joel Timothy Oh <joeltimothyoh@live.com>
2019-03-29 00:06:30 +0800

    test Win32_Logicaldisk

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,15 +1,7 @@
 begin {
-    $projectUrl = 'https://github.com/joeltimothyoh/Get-SMARTReport.git'
-    $projectName = [System.IO.Path]::GetFileNameWithoutExtension($projectUrl)
 }process {
     try {
-        # Clone project
-        git clone "$projectUrl"; if ($LASTEXITCODE) { throw }
-        Push-Location "$projectName"
-        Get-ChildItem -Force | Format-Table
-        # Test project
-        Import-Module "./Modules/$($projectName)/Get-SMARTReport.psm1"
-        Get-SMARTReport
+        Get-WmiObject -Class Win32_Logicaldisk
     }catch {
         throw
     }
