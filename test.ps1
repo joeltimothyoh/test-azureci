d9ac09f92dbf87f633fcd0b30de911b3509f5d29
Joel Timothy Oh <joeltimothyoh@live.com>
2019-03-28 21:44:59 +0800

    once more

--- test.ps1
+++ test.ps1
@@ -2,9 +2,11 @@ begin {
     $projectName = 'Get-SMARTReport'
 }process {
     try {
+        # Clone project
         git clone "https://github.com/joeltimothyoh/$($projectName).git"; if ($?) { throw }
         Push-Location $projectName
-        Get-Location
+        Get-ChildItem -Force | Format-Table
+        # Test project
         Import-Module "./Modules/$($projectName)/Get-SMARTReport.psm1"
         Get-SMARTReport
     }catch {
