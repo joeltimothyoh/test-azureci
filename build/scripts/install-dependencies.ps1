28cc7dffbeef541e75fafeff5e636e63f88132e9
Joel Timothy Oh <joeltimothyoh@live.com>
2019-09-30 04:51:39 +0800

    Add install-dependencies.ps1 and Install Dependencies build step

---
+++ build/scripts/install-dependencies.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param()
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+# Install PowershellGet module of the specified version
+$powershellGetRequiredVersion = '2.1.2'
+$powershellGetInstalledVersions = (Get-Module PowerShellGet -ListAvailable).Version | % { $_.ToString() }
+if ($powershellGetRequiredVersion -notin $powershellGetInstalledVersions) {
+    Install-Module -Name PowershellGet -Repository 'PSGallery' -RequiredVersion $powershellGetRequiredVersion -Scope CurrentUser -Force -Verbose -AllowClobber
+}
+# Import and get info on PowershellGet
+Import-Module -Name PowerShellGet -RequiredVersion $powershellGetRequiredVersion -Force -Verbose
+Get-Module -Name PowerShellGet -ListAvailable
