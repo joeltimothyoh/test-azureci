855319c420a196a299254839ee5e0760a72512da
Joel Timothy Oh <joeltimothyoh@live.com>
2019-09-30 08:28:15 +0800

    publish-module.ps1: Add parameter -Repository, improve checks for path, add Write-Verbose lines, improve determination of module's directory based on path specified

--- build/scripts/publish-module.ps1
+++ build/scripts/publish-module.ps1
@@ -3,19 +3,30 @@ param(
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     [string]$Path
+    ,
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Repository
 )
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
+"Path: $Path" | Write-Verbose
+Get-Item -Path $Path > $null
+
+if (Test-Path -Path $Path -Leaf) {
+    $modulesDir = Split-Path -Path $Path -Parent
+    "Module directory determined to be '$modulesDir'" | Write-Verbose
+}
 if (!$env:MODULE_VERSION) {
     "Module version is null. Not publishing module." | Write-Warning
     return
 }
 
-"Version: $env:MODULE_VERSION"
-$manifest = Test-ModuleManifest -Path $Path
+"Module manifest path: $modulesDir" | Write-Verbose
+"Version: $env:MODULE_VERSION" | Write-Verbose
 
 # Publish the module
-"Publishing module: '$($manifest.Name)'" | Write-Host
-Publish-Module -Path (Split-Path -Path $Path -Parent) -NuGetApiKey $env:NUGET_API_KEY
+"Publishing module" | Write-Host
+Publish-Module -Path $modulesDir -Repository $Repository -NuGetApiKey $env:NUGET_API_KEY
