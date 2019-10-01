87b31fbefbdf30afc659dcba3a2f66e9af8741d4
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-01 09:09:55 +0800

    Add example test.ps1

--- tests/test.ps1
+++ tests/test.ps1
@@ -2,3 +2,13 @@
 param()
 
 "Running tests.ps1" | Write-Host
+
+Set-StrictMode -Version Latest
+$global:PesterDebugPreference_ShowFullErrors = $true
+
+# Install Pester if needed
+$pester = Get-Module Pester -ListAvailable -ErrorAction SilentlyContinue
+if ( ! $pester -or $pester.Version.Major -lt 4 ) {
+    Install-Module Pester -Force -Scope CurrentUser
+}
+Get-Module Pester -ListAvailable
